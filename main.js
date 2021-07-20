4dafef7ceecf4222ee8a9312f92456b184c6315e
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2021-07-20 16:46:56 -0400

    eventListener on click

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 var button = document.getElementsByTagName('button')[0];
+
+var clickFunction = function () {
+  console.log('click, click, boom!');
+};
+
+button.addEventListener('click', clickFunction);
