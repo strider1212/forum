fcee799ef32e4843d1821fd2f5fb7724788f8e9d
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2021-07-20 17:27:01 -0400

    message posts in text

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ var button = document.getElementsByTagName('button')[0];
 var clickFunction = function () {
   var posts = document.getElementsByClassName('posts')[0];
   var name = document.getElementById('name').value;
+  var message = document.getElementById('message').value;
 
-  posts.innerHTML = `Posted by ${name}`;
+  posts.innerHTML = `Posted by ${name} - ${message}`;
 };
 
 button.addEventListener('click', clickFunction);
