9d8fd4bfa375cd101c0f4af54468473922b7ed87
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2021-07-20 17:16:51 -0400

    var name

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var button = document.getElementsByTagName('button')[0];
 
 var clickFunction = function () {
   var posts = document.getElementsByClassName('posts')[0];
+  var name = document.getElementById('name');
 };
 
 button.addEventListener('click', clickFunction);
