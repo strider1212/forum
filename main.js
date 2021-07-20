d4dc95b23ae2186306e2c3b6fc231a7bd691fa18
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2021-07-20 17:13:30 -0400

    change forum to post

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var button = document.getElementsByTagName('button')[0];
 
 var clickFunction = function () {
-  var forum = document.getElementsByClassName('posts')[0];
+  var posts = document.getElementsByClassName('posts')[0];
 };
 
 button.addEventListener('click', clickFunction);
