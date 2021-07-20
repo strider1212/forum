8a0cb187ee9527fabf602770096d434892d6f518
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2021-07-20 17:18:27 -0400

    add value to name

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var button = document.getElementsByTagName('button')[0];
 
 var clickFunction = function () {
   var posts = document.getElementsByClassName('posts')[0];
-  var name = document.getElementById('name');
+  var name = document.getElementById('name').value;
 };
 
 button.addEventListener('click', clickFunction);
